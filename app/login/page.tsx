5a9b024d23e6c28bc824d3581da8cd66b2fd5f0e
MarcioA <zamar_pinheiro@hotmail.com>
2023-12-01 00:52:45 -0300

    adding style to form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,10 @@
 import LoginForm from '@/app/ui/login-form';
+import styles from '@/app/ui/login.module.css';
 
 export default function LoginPage() {
     return (
-        <main className="flex items-center justify-center md:h-screen">
-            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <main className={styles.container}>
+            <div className={styles['form-group']}>
                 <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
                     <div className="w-32 text-white md:w-36">
                     </div>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -2,7 +2,6 @@
 
 import { lusitana } from '@/app/ui/fonts';
 import {
-  AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
@@ -10,46 +9,46 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
+import styles from './login.module.css';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form action={dispatch} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+    <form action={dispatch} className={styles['form-group']}>
+      <div className={styles.container}>
+        <h1 className={`${lusitana.className} ${styles.h2}`}>
           Por favor, faça login para seguir.
         </h1>
-        <div className="w-full">
+        <div className={styles['form-group']}>
           <div>
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className={styles.label}
               htmlFor="email"
             >
               Email
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className={styles.input}
                 id="email"
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
                 required
               />
-              {/* <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
             </div>
           </div>
           <div className="mt-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className={styles.label}
               htmlFor="password"
             >
               Senha
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className={`${styles.input} ${styles['password-container']}`}
                 id="password"
                 type="password"
                 name="password"
@@ -57,7 +56,6 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
@@ -86,7 +84,7 @@ function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
+    <Button className={styles.button} aria-disabled={pending}>
       Login <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
